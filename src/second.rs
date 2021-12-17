dcb2322e774a2b58d580f8c580c977eed8198c8b
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-12-17 23:07:38 +0200

    Implement second iteration of stack using Option enum

---
+++ src/second.rs
@@ -0,0 +1,79 @@
+use std::mem;
+
+pub struct List {
+  head: Link,
+}
+
+type Link = Option<Box<Node>>;
+
+struct Node {
+  elem: i32,
+  next: Link,
+}
+
+impl List {
+  pub fn new() -> Self {
+    List { head: None }
+  }
+
+  pub fn push(&mut self, elem: i32) {
+    let node = Box::new(Node {
+      elem,
+      next: self.head.take(),
+    });
+
+    self.head = Some(node);
+  }
+
+  pub fn pop(&mut self) -> Option<i32> {
+    self.head.take().map(|node| {
+      self.head = node.next;
+      node.elem
+    })
+  }
+}
+
+impl Drop for List {
+  fn drop(&mut self) {
+    let mut cur_link = mem::replace(&mut self.head, None);
+    // `while let` == "do this thing until this pattern doen't match"
+    while let Some(mut boxed_node) = cur_link {
+      cur_link = boxed_node.next.take();
+      // boxed_node goes out of scope and gets dropped here;
+      // but its Node's `next` field has been set to None
+      // so no unbounded recursion occurs.
+    }
+  }
+}
+
+#[cfg(test)]
+mod test {
+  use super::List;
+
+  #[test]
+  fn basics() {
+    let mut list = List::new();
+
+    // Check emply list behaves right
+    assert_eq!(list.pop(), None);
+    // Populate list
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    // Check normal removal
+    assert_eq!(list.pop(), Some(3));
+    assert_eq!(list.pop(), Some(2));
+    // Push some more just to make sure nothing's corrupted
+    list.push(4);
+    list.push(5);
+
+    // Check normal removal
+    assert_eq!(list.pop(), Some(5));
+    assert_eq!(list.pop(), Some(4));
+
+    // Check exhaustion
+    assert_eq!(list.pop(), Some(1));
+    assert_eq!(list.pop(), None);
+  }
+}
