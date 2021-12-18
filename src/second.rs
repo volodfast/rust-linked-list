8abecb896131baee95d4e2b1e7507d9fe6f3f214
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-12-18 23:45:16 +0200

    Implement iter function for second implementation of stack

--- src/second.rs
+++ src/second.rs
@@ -73,6 +73,29 @@ impl<T> Iterator for IntoIter<T> {
   }
 }
 
+pub struct Iter<'a, T> {
+  next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+  pub fn iter<'a>(&'a self) -> Iter<'a, T> {
+    Iter {
+      next: self.head.as_deref(),
+    }
+  }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+  type Item = &'a T;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    self.next.map(|node| {
+      self.next = node.next.as_deref();
+      &node.elem
+    })
+  }
+}
+
 #[cfg(test)]
 mod test {
   use super::List;
