79a238d3e4fd1e705b15ed4bc958c60dc34d0735
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-12-19 00:15:46 +0200

    Add test for iter_mut functionality

--- src/second.rs
+++ src/second.rs
@@ -198,4 +198,19 @@ mod test {
     assert_eq!(iter.next(), Some(&2));
     assert_eq!(iter.next(), Some(&1));
   }
+
+  #[test]
+  fn iter_mut() {
+    let mut list = List::new();
+
+    list.push(1);
+    list.push(2);
+    list.push(3);
+
+    let mut iter_mut = list.iter_mut();
+
+    assert_eq!(iter_mut.next(), Some(&mut 3));
+    assert_eq!(iter_mut.next(), Some(&mut 2));
+    assert_eq!(iter_mut.next(), Some(&mut 1));
+  }
 }
