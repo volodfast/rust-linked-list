53d0164606aab7dc44ee2e26a6235688edaa52c7
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-12-20 22:59:59 +0200

    Add tests to persistent stack implementation

--- src/third.rs
+++ src/third.rs
@@ -35,3 +35,29 @@ impl<T> List<T> {
     self.head.as_ref().map(|node| &node.elem)
   }
 }
+
+#[cfg(test)]
+mod test {
+  use super::List;
+
+  #[test]
+  fn basics() {
+    let list = List::new();
+    assert_eq!(list.head(), None);
+
+    let list = list.prepend(1).prepend(2).prepend(3);
+    assert_eq!(list.head(), Some(&3));
+
+    let list = list.tail();
+    assert_eq!(list.head(), Some(&2));
+
+    let list = list.tail();
+    assert_eq!(list.head(), Some(&1));
+
+    let list = list.tail();
+    assert_eq!(list.head(), None);
+
+    let list = list.tail();
+    assert_eq!(list.head(), None);
+  }
+}
