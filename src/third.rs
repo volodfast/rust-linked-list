774f84728801f896bbef96c672b762e96bc59369
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-12-20 23:05:47 +0200

    Add iter to persistent stack implementation

--- src/third.rs
+++ src/third.rs
@@ -36,6 +36,29 @@ impl<T> List<T> {
   }
 }
 
+pub struct Iter<'a, T> {
+  next: Option<&'a Node<T>>,
+}
+
+impl<T> List<T> {
+  pub fn iter(&self) -> Iter<'_, T> {
+    Iter {
+      next: self.head.as_deref(),
+    }
+  }
+}
+
+impl<'a, T> Iterator for Iter<'a, T> {
+  type Item = &'a T;
+
+  fn next(&mut self) -> Option<Self::Item> {
+    self.next.map(|node| {
+      self.next = node.next.as_deref();
+      &node.elem
+    })
+  }
+}
+
 #[cfg(test)]
 mod test {
   use super::List;
@@ -59,5 +82,16 @@ mod test {
 
     let list = list.tail();
     assert_eq!(list.head(), None);
+  }
+
+  #[test]
+  fn iter() {
+    let list = List::new().prepend(1).prepend(2).prepend(3);
+
+    let mut iter = list.iter();
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&1));
+    assert_eq!(iter.next(), None);
   }
 }
