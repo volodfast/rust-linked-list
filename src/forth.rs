f9918cf6d920df40b270a207df5ad6538053a57c
Volodymyr Bystranivskyi <volodfast@gmail.com>
2021-12-21 23:53:12 +0200

    Implement IntoIter for doubly linked list

--- src/forth.rs
+++ src/forth.rs
@@ -142,6 +142,28 @@ impl<T> Drop for List<T> {
   }
 }
 
+pub struct IntoIter<T>(List<T>);
+
+impl<T> List<T> {
+  pub fn into_iter(self) -> IntoIter<T> {
+    IntoIter(self)
+  }
+}
+
+impl<T> Iterator for IntoIter<T> {
+  type Item = T;
+
+  fn next(&mut self) -> Option<T> {
+    self.0.pop_front()
+  }
+}
+
+impl<T> DoubleEndedIterator for IntoIter<T> {
+  fn next_back(&mut self) -> Option<T> {
+    self.0.pop_back()
+  }
+}
+
 #[cfg(test)]
 mod test {
   use super::List;
@@ -215,5 +237,22 @@ mod test {
     assert_eq!(&mut *list.peek_front_mut().unwrap(), &mut 3);
     assert_eq!(&*list.peek_back().unwrap(), &1);
     assert_eq!(&mut *list.peek_back_mut().unwrap(), &mut 1);
+  }
+
+  #[test]
+  fn into_iter() {
+    let mut list = List::new();
+
+    list.push_front(1);
+    list.push_front(2);
+    list.push_front(3);
+
+    let mut iter = list.into_iter();
+
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next_back(), Some(1));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next_back(), None);
+    assert_eq!(iter.next(), None);
   }
 }
